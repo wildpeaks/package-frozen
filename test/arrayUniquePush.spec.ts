d89a2f8cc5ccef2ce92e085d2d8f1e0a3d1f5984
Cecile Muller <cecilemuller@users.noreply.github.com>
2019-11-30 07:31:00 +0100

    test: Convert tests to Mocha

--- test/arrayUniquePush.spec.ts
+++ test/arrayUniquePush.spec.ts
@@ -1,5 +1,8 @@
-/* eslint-env node, jasmine */
+/* eslint-env node */
+import {strictEqual, deepStrictEqual} from 'assert';
+import {describe, it} from 'mocha';
 import {arrayUniquePush} from '../src/frozen';
+
 
 describe('arrayUniquePush', () => {
 	type MyArray = ReadonlyArray<string | number>;
@@ -7,26 +10,26 @@ describe('arrayUniquePush', () => {
 	it('New value', () => {
 		const initial: MyArray = Object.freeze(['zero', 111, 'TWO']);
 		const actual: MyArray = arrayUniquePush(initial, 333);
-		expect(initial).toEqual(['zero', 111, 'TWO'], 'Initial remains unchanged');
-		expect(Array.isArray(actual)).toBe(true, 'Actual is an Array');
-		expect(Object.isFrozen(actual)).toBe(true, 'Actual is frozen');
-		expect(actual).toEqual(['zero', 111, 'TWO', 333], 'Actual added the new value');
+		deepStrictEqual(initial, ['zero', 111, 'TWO'], 'Initial remains unchanged');
+		strictEqual(Array.isArray(actual), true, 'Actual is an Array');
+		strictEqual(Object.isFrozen(actual), true, 'Actual is frozen');
+		deepStrictEqual(actual, ['zero', 111, 'TWO', 333], 'Actual added the new value');
 	});
 	it('Existing value', () => {
 		const initial: MyArray = Object.freeze(['zero', 111, 'TWO']);
 		const actual: MyArray = arrayUniquePush(initial, 111);
-		expect(initial).toEqual(['zero', 111, 'TWO'], 'Initial remains unchanged');
-		expect(Array.isArray(actual)).toBe(true, 'Actual is an Array');
-		expect(Object.isFrozen(actual)).toBe(true, 'Actual is frozen');
-		expect(actual).toEqual(['zero', 111, 'TWO'], `Actual didn't duplicate the new value`);
+		deepStrictEqual(initial, ['zero', 111, 'TWO'], 'Initial remains unchanged');
+		strictEqual(Array.isArray(actual), true, 'Actual is an Array');
+		strictEqual(Object.isFrozen(actual), true, 'Actual is frozen');
+		deepStrictEqual(actual, ['zero', 111, 'TWO'], `Actual didn't duplicate the new value`);
 	});
 	it('NaN', () => {
 		const initial: MyArray = Object.freeze(['zero', 111, 'TWO']);
 		const actual: MyArray = arrayUniquePush(initial, NaN);
-		expect(initial).toEqual(['zero', 111, 'TWO'], 'Initial remains unchanged');
-		expect(Array.isArray(actual)).toBe(true, 'Actual is an Array');
-		expect(Object.isFrozen(actual)).toBe(true, 'Actual is frozen');
-		expect(actual).toEqual(['zero', 111, 'TWO', NaN], 'Actual added the new value');
+		deepStrictEqual(initial, ['zero', 111, 'TWO'], 'Initial remains unchanged');
+		strictEqual(Array.isArray(actual), true, 'Actual is an Array');
+		strictEqual(Object.isFrozen(actual), true, 'Actual is frozen');
+		deepStrictEqual(actual, ['zero', 111, 'TWO', NaN], 'Actual added the new value');
 	});
 
 	//-----------------------------------------------------------------------//
@@ -35,41 +38,41 @@ describe('arrayUniquePush', () => {
 	// it('undefined', () => {
 	// 	const initial: MyArray = Object.freeze(['zero', 111, 'TWO']);
 	// 	const actual: MyArray = arrayUniquePush(initial, undefined); // This should be a syntax error
-	// 	expect(initial).toEqual(['zero', 111, 'TWO'], 'Initial remains unchanged');
-	// 	expect(Array.isArray(actual)).toBe(true, 'Actual is an Array');
-	// 	expect(Object.isFrozen(actual)).toBe(true, 'Actual is frozen');
-	// 	expect(actual).toEqual(['zero', 111, 'TWO', undefined], 'Actual added the new value');
+	// 	deepStrictEqual(initial, ['zero', 111, 'TWO'], 'Initial remains unchanged');
+	// 	strictEqual(Array.isArray(actual), true, 'Actual is an Array');
+	// 	strictEqual(Object.isFrozen(actual), true, 'Actual is frozen');
+	// 	deepStrictEqual(actual, ['zero', 111, 'TWO', undefined], 'Actual added the new value');
 	// });
 	// it('null', () => {
 	// 	const initial: MyArray = Object.freeze(['zero', 111, 'TWO']);
 	// 	const actual: MyArray = arrayUniquePush(initial, null); // This should be a syntax error
-	// 	expect(initial).toEqual(['zero', 111, 'TWO'], 'Initial remains unchanged');
-	// 	expect(Array.isArray(actual)).toBe(true, 'Actual is an Array');
-	// 	expect(Object.isFrozen(actual)).toBe(true, 'Actual is frozen');
-	// 	expect(actual).toEqual(['zero', 111, 'TWO', null], 'Actual added the new value');
+	// 	deepStrictEqual(initial, ['zero', 111, 'TWO'], 'Initial remains unchanged');
+	// 	strictEqual(Array.isArray(actual), true, 'Actual is an Array');
+	// 	strictEqual(Object.isFrozen(actual), true, 'Actual is frozen');
+	// 	deepStrictEqual(actual, ['zero', 111, 'TWO', null], 'Actual added the new value');
 	// });
 	// it('{}', () => {
 	// 	const initial: MyArray = Object.freeze(['zero', 111, 'TWO']);
 	// 	const actual: MyArray = arrayUniquePush(initial, {}); // This should be a syntax error
-	// 	expect(initial).toEqual(['zero', 111, 'TWO'], 'Initial remains unchanged');
-	// 	expect(Array.isArray(actual)).toBe(true, 'Actual is an Array');
-	// 	expect(Object.isFrozen(actual)).toBe(true, 'Actual is frozen');
-	// 	expect(actual).toEqual(['zero', 111, 'TWO', {}], 'Actual added the new value');
+	// 	deepStrictEqual(initial, ['zero', 111, 'TWO'], 'Initial remains unchanged');
+	// 	strictEqual(Array.isArray(actual), true, 'Actual is an Array');
+	// 	strictEqual(Object.isFrozen(actual), true, 'Actual is frozen');
+	// 	deepStrictEqual(actual, ['zero', 111, 'TWO', {}], 'Actual added the new value');
 	// });
 	// it('true', () => {
 	// 	const initial: MyArray = Object.freeze(['zero', 111, 'TWO']);
 	// 	const actual: MyArray = arrayUniquePush(initial, true); // This is a syntax error as expected
-	// 	expect(initial).toEqual(['zero', 111, 'TWO'], 'Initial remains unchanged');
-	// 	expect(Array.isArray(actual)).toBe(true, 'Actual is an Array');
-	// 	expect(Object.isFrozen(actual)).toBe(true, 'Actual is frozen');
-	// 	expect(actual).toEqual(['zero', 111, 'TWO', true], 'Actual added the new value');
+	// 	deepStrictEqual(initial, ['zero', 111, 'TWO'], 'Initial remains unchanged');
+	// 	strictEqual(Array.isArray(actual), true, 'Actual is an Array');
+	// 	strictEqual(Object.isFrozen(actual), true, 'Actual is frozen');
+	// 	deepStrictEqual(actual, ['zero', 111, 'TWO', true], 'Actual added the new value');
 	// });
 	// it('false', () => {
 	// 	const initial: MyArray = Object.freeze(['zero', 111, 'TWO']);
 	// 	const actual: MyArray = arrayUniquePush(initial, false); // This is a syntax error as expected
-	// 	expect(initial).toEqual(['zero', 111, 'TWO'], 'Initial remains unchanged');
-	// 	expect(Array.isArray(actual)).toBe(true, 'Actual is an Array');
-	// 	expect(Object.isFrozen(actual)).toBe(true, 'Actual is frozen');
-	// 	expect(actual).toEqual(['zero', 111, 'TWO', false], 'Actual added the new value');
+	// 	deepStrictEqual(initial, ['zero', 111, 'TWO'], 'Initial remains unchanged');
+	// 	strictEqual(Array.isArray(actual), true, 'Actual is an Array');
+	// 	strictEqual(Object.isFrozen(actual), true, 'Actual is frozen');
+	// 	deepStrictEqual(actual, ['zero', 111, 'TWO', false], 'Actual added the new value');
 	// });
 });
