f84e5de24ba451daad6222256e5628e69708fead
Cecile Muller <cecilemuller@users.noreply.github.com>
2019-12-18 14:35:22 +0100

    chore: add Prettier & tweak settings

--- wallaby.js
+++ wallaby.js
@@ -1,15 +1,11 @@
-'use strict';
+"use strict";
 
 module.exports = () => ({
 	debug: true,
-	testFramework: 'jasmine',
-	files: [
-		'src/**/*.ts'
-	],
-	tests: [
-		'test/*.spec.ts'
-	],
+	testFramework: "jasmine",
+	files: ["src/**/*.ts"],
+	tests: ["test/*.spec.ts"],
 	env: {
-		type: 'node'
+		type: "node"
 	}
 });
